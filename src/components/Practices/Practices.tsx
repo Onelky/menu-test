8e4c86e84304fa4ab70ae1596639e0d505c73e5c
onelky <onelky.hernandez29@gmail.com>
2023-10-13 21:10:04 -0400

    add CommonSelect to Practices

--- src/components/Practices/Practices.tsx
+++ src/components/Practices/Practices.tsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import { Stack } from '@mui/material'
+import { CustomSelect } from '../common/Select'
 
 export const Practices = () => {
-  return <div></div>
+  return (
+    <Stack>
+      <CustomSelect
+        label={'Display By'}
+        defaultValue={'practice'}
+        options={[
+          { label: 'Practice', value: 'practice' },
+          { label: 'Other', value: 'other' }
+        ]}
+      />
+    </Stack>
+  )
 }
---
+++ src/components/common/Select/Select.tsx
@@ -0,0 +1,48 @@
+import React, { type FC, useState } from 'react'
+import Select, { type SelectChangeEvent } from '@mui/material/Select'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import PlaceOutlined from '@mui/icons-material/PlaceOutlined'
+import KeyboardArrowDownOutlined from '@mui/icons-material/KeyboardArrowDownOutlined'
+import { useTheme } from '@mui/material/styles'
+import type { Option } from '@app/types'
+
+type SelectProps = {
+  label: string
+  defaultValue?: string
+  options: Option[]
+}
+export const CustomSelect: FC<SelectProps> = ({
+  label,
+  defaultValue = '',
+  options
+}) => {
+  const theme = useTheme()
+  const [value, setValue] = useState(defaultValue ?? '')
+  const handleChange = ({ target: { value } }: SelectChangeEvent) =>
+    setValue(value)
+
+  return (
+    <FormControl fullWidth>
+      <InputLabel id="select">{label}</InputLabel>
+      <Select
+        labelId="select-label"
+        id="select"
+        value={value}
+        label={label}
+        onChange={handleChange}
+        defaultValue={defaultValue}
+        startAdornment={<PlaceOutlined color={'secondary'} />}
+        IconComponent={KeyboardArrowDownOutlined}
+        sx={{ 'svg path': { fill: theme.palette.secondary.main } }}
+      >
+        {options.map(({ value, label }) => (
+          <MenuItem key={value} value={value}>
+            {label}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  )
+}
