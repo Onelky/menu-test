a74fcb6de6c6faa0643d6b2d1c5e2149115225b2
onelky <onelky.hernandez29@gmail.com>
2023-10-14 22:03:21 -0400

    create filterPractices test

---
+++ src/components/Practices/__tests__/filterPractices.test.jsx
@@ -0,0 +1,33 @@
+import userEvent from '@testing-library/user-event'
+import { render, screen, waitFor } from '@testing-library/react'
+import { Practices } from '../Practices'
+
+test('rows are filtered based on search term (case insensitive)', async () => {
+  const user = userEvent.setup()
+  render(<Practices />)
+
+  const searchBar = screen.getByPlaceholderText('Search')
+  await waitFor(async () => {
+    await user.clear(searchBar)
+    await user.type(searchBar, 'Northside')
+  })
+
+  // find items that match search term
+  expect(
+    await screen.findAllByRole('cell', {
+      name: /NORTHSIDE/i
+    })
+  ).toHaveLength(2)
+
+  // check that there are no matches with an invalid search term
+  await waitFor(async () => {
+    await user.clear(searchBar)
+    await user.type(searchBar, 'No match')
+  })
+  expect(
+    screen.queryAllByRole('cell', {
+      name: /No match/i
+    })
+  ).toHaveLength(0)
+  expect(screen.getByText('No data found')).toBeInTheDocument()
+})
